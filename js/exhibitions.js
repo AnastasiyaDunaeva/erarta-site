5bd3aae1aaf755641233f47b32e60ae5268f9f0d
Anastasiya <dunaeva.anastasiya.1989@mail.ru>
2020-09-29 21:45:17 +0300

    exhibitions

--- js/exhibitions.js
+++ js/exhibitions.js
@@ -1,16 +1,18 @@
 //isotope
 
 let $grid = $(".grid").isotope({
-    itemSelector: ".grid-item",
-    percentPosition: true,
-    masonry: {
-        columnWidth: ".grid-item",
-    },
+  itemSelector: ".grid-item",
+  percentPosition: true,
+  masonry: {
+    columnWidth: ".grid-item",
+  },
 });
 // filter items on button click
 $(".filter-button-group").on("click", "button", function () {
-    let filterValue = $(this).attr("data-filter");
-    $grid.isotope({
-        filter: filterValue,
-    });
+  $(".filter-button-group button.active").removeClass("active");
+  $(this).addClass("active");
+  let filterValue = $(this).attr("data-filter");
+  $grid.isotope({
+    filter: filterValue,
+  });
 });
